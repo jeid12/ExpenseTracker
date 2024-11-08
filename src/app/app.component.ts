b1dc4fa1d717254d645fcd4d9aaf8ea58d5e4d0b
jeid12 <niyokwizerajd123@gmail.com>
2024-11-08 17:30:46 +0200

    updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NzLayoutModule } from 'ng-zorro-antd/layout';  // Import the NzLayoutModule
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NzLayoutModule],  // Add NzLayoutModule here
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']  // Corrected styleUrl to styleUrls for proper Angular syntax
 })
 export class AppComponent {
   title = 'expenseFront';
