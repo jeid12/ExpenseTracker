afc68ad79779876a5b35cd9d2ebf499923a81b31
jeid12 <niyokwizerajd123@gmail.com>
2024-11-09 10:24:16 +0200

    update the route

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,10 +4,12 @@ import { DashboardComponent } from './components/dashboard/dashboard.component';
 import { IncomeComponent } from './components/income/income.component';
 import { ExpenseComponent } from './components/expense/expense.component';
 import { UpdateExpenseComponent } from './components/update-expense/update-expense.component';
+import { UpdateIncomeComponent } from './components/update-income/update-income.component';
 
 export const routes: Routes = [
   { path: '', component: DashboardComponent }, // Default route
   { path: 'income', component: IncomeComponent },
   { path: 'expense', component: ExpenseComponent }, // Expense route
   { path: 'expense/:id/edit', component: UpdateExpenseComponent }, // Expense update Route
+  { path: 'income/:id/edit', component: UpdateIncomeComponent }, // Income update Route
 ];
