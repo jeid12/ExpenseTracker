41e77bca64f9183712c5a1d88fa4d52b4bd8c568
jeid12 <niyokwizerajd123@gmail.com>
2024-11-09 10:22:02 +0200

    update post income

--- src/app/components/income/income.component.ts
+++ src/app/components/income/income.component.ts
@@ -9,6 +9,7 @@ import { NzIconModule } from 'ng-zorro-antd/icon';
 import { NzInputModule } from 'ng-zorro-antd/input';
 import { NzMessageService } from 'ng-zorro-antd/message';
 import { NzSelectModule } from 'ng-zorro-antd/select';
+import { IncomeService } from '../../services/income/income.service';
 
 @Component({
   selector: 'app-income',
@@ -18,18 +19,25 @@ import { NzSelectModule } from 'ng-zorro-antd/select';
   styleUrl: './income.component.scss'
 })
 export class IncomeComponent {
-postIncome() {
+deleteIncome(arg0: any) {
 throw new Error('Method not implemented.');
 }
+updateIncome(arg0: any) {
+throw new Error('Method not implemented.');
+}
+incomes: any[] = []; // Ensure expenses is an array
+
 incomeForm!: FormGroup;
 listofCategory: any[] = ['salary', 'business', 'gift','loan','family','youtube', 'other'];
 
 constructor(private fb: FormBuilder,
   private message: NzMessageService,
-  private router: Router
+  private router: Router,
+  private incomeService: IncomeService,
 ) {}
 
 ngOnInit() {
+  this.getAllIncome(); // Fetch expenses when the component initializes
   this.incomeForm = this.fb.group({
     title: [null, Validators.required],
     amount: [null, Validators.required],
@@ -38,5 +46,29 @@ ngOnInit() {
     decription: [null, Validators.required],
   });
 }
+submitForm() {  
+  if (this.incomeForm.invalid) {
+    return; // Prevent submission if the form is invalid
+  }
+  this.incomeService.postIncome(this.incomeForm.value).subscribe(
+    (res) => {
+      this.message.success('Income added successfully', { nzDuration: 5000 });
+      this.getAllIncome();
+    },
+    (error) => {
+      this.message.error('Error adding income', { nzDuration: 5000 });
+    }
+  );  
+}
+
+getAllIncome() {
+  this.incomeService.getAllIncome().subscribe(  
+    (res) => {
+      this.incomes= res;
+    }, (error) => { 
+      this.message.error('Error fetching income details', { nzDuration: 5000 });
+    } 
+  );
+  }
 
 }
